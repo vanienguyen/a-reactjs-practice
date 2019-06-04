7d9ee6b584bedd65cbcb24962194bd4700a3f317
Vanie Nguyen <vanienguyen2412@gmail.com>
2019-06-04 17:00:53 +1200

    make MiniCart component pops up when "My Cart" is mouseovered

---
+++ front-end-test-from-moustache-republic/src/PageComponents/MiniCart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './ClassicTeePage.css';
+
+class MiniCart extends React.Component {
+
+    render() {
+        return (
+            <div className='mini-cart'>
+                Mini Cart
+            </div>
+        );
+    }
+}
+
+export default MiniCart;
--- front-end-test-from-moustache-republic/src/PageComponents/NavBar.js
+++ front-end-test-from-moustache-republic/src/PageComponents/NavBar.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import './ClassicTeePage.css';
+import MiniCart from "./MiniCart";
 
 class NavBar extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.cartOnMouseOver = this.cartOnMouseOver.bind(this);
+
+        this.state = {cartOnMouseOver: false}
+
+    }
+
+    cartOnMouseOver() {
+        this.setState({cartOnMouseOver: !this.state.cartOnMouseOver})
+    }
 
     render() {
         const numOfItems = 0;
+        let miniCart;
+
         return (
-            <div className='nav-bar'>
-                <div> My Cart ({numOfItems})</div>
+            <div className="dropdown" style={{float: 'right'}}>
+                <button className="dropbtn">My Cart ({numOfItems})</button>
+                <div className="dropdown-content">
+                    <div>
+                        <MiniCart />
+                    </div>
+                </div>
             </div>
         );
     }
